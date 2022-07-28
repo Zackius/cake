96352a75f4a987b106d56b8cfc38bfc7b9244054
Zacharia <ndunguzachary24@gmail.com>
2022-07-28 09:40:53 +0300

    updated features

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  function handleClick() {
+    setClick(!click);
+  }
   return (
     <nav className="navbar">
       <Link to="/" className="navlogo">
-        <div>Balmy Bakes</div>
+        Balmy Bakes
       </Link>
-      <nav className="navbar-container">
-        <Link to="/" className="nav-link">
-          Cakes
-        </Link>
-        <Link to="/" className="nav-link">
-          Wedding Cakes
-        </Link>
-        <Link to="/" className="nav-link">
-          Birthday Cakes
-        </Link>
-        <Link to="/" className="nav-link">
-          PrIce
-        </Link>
-        <Link to="/" className="nav-link">
-          Order
-        </Link>
-      </nav>
+      <div className="nav-icon" onClick={handleClick}>
+        <i className={click ? "fas fas-times" : "fas fa-bars"} />
+      </div>
+      <div className={click ? "nav-icon active" : " nav-icon"}>
+        <div className="navbar-container">
+          <Link to="/" className="nav-link">
+            Cakes
+          </Link>
+          <Link to="/" className="nav-link">
+            Wedding Cakes
+          </Link>
+          <Link to="/" className="nav-link">
+            Birthday Cakes
+          </Link>
+          <Link to="/" className="nav-link">
+            PrIce
+          </Link>
+          <Link to="/" className="nav-link">
+            Order
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 };
