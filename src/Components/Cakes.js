ab86fb8d0f55fe5ba9dc4073d5e111d87c0fc739
Zacharia <ndunguzachary24@gmail.com>
2022-07-29 04:46:56 +0300

    fetching data

--- src/Components/Cakes.js
+++ src/Components/Cakes.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Birthday = () => {
-  return <div>Birthday</div>;
+  const [cakes, setCakes] = useState("");
+
+  useEffect(() => {
+    getCakes();
+  }, []);
+
+  function getCakes() {
+    axios
+      .get("http://localhost:3000/cakes")
+      .then((resp) => {
+        const allCakes = resp.data;
+        setCakes(allCakes);
+        console.log();
+      })
+      .catch((error) => console.error(`Error $(error)`));
+  }
+  console.log(cakes);
+  return { cakes };
 };
 
 export default Birthday;
--- src/Components/Order.js
+++ src/Components/Order.js
@@ -3,22 +3,25 @@ import "./Order.css";
 
 const Order = () => {
   return (
-    <div>
-      <div>
-        <form>
+    <div className="main">
+      <div className="inputOrder">
+        <form className="list">
           <label>
-            {" "}
-            Cakes Flavor:
-            <input type="text" />
+            <h4>Cakes Flavor</h4>
+            <input className="gap" type="text" />
           </label>
           <label>
-            Image
-            <input type="url" />{" "}
+            <h4> Image-Url</h4>
+            <input className="gap" type="url" />{" "}
           </label>
           <label>
-            Number of KGs
-            <input type="number" />
+            <h4> Number of KGs</h4>
+            <input className="gap" type="number" />
           </label>
+          <div className="btn">
+          <button>Submit</button>
+          </div>
+         
         </form>
       </div>
     </div>
