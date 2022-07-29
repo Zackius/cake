f446689a4b056a197b7c8767b71ef6e303275163
Zacharia <ndunguzachary24@gmail.com>
2022-07-29 07:28:35 +0300

    updating cake list

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -1,48 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Order.css";
 
 const Order = () => {
+  const [cakes, setCakes] = useState([]);
+  const [cakeName, setCakeName] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [numberOfKgs, setNumberOfKgs] = useState("");
+  const[cakesId, setCakesId] = useState(null)
 
-  function handleCakeFlavor(e){
-    setUserFirstName(e.target.value)
-   }
-  
-   function handleImageUrl(e){
-    setUserLastName(e.target.value)
-   }
-  
-   function handleNumber(e){
-    setUserBio(e.target.value)
-   }
-   
-   function handleSubmit(e){
-    e.preventDefault()
-     const cakeData = {
-       first userFirstName,
-       lastName: userLastName,
-       About: userBio
-     }
+  useEffect(() => {
+    getCakes();
+  }, [])
+  function updateCakes() {
+    let item = { cakeName, imageUrl, numberOfKgs };
+
+    fetch(`http://localhost:3000/cakes${cakesId}`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item),
+    }).then((result) => {
+      result.json().then((resp) => {
+        getUsers();
+      });
+    });
+  }
 
   return (
     <div className="main">
       <div className="inputOrder">
-        <form className="list"  onClick={handleSubmit}>
+        <form className="list" onClick={handleSubmit}>
           <label>
             <h4>Cakes Flavor</h4>
-            <input className="gap" type="text"  onChange={handleCakeFlavor}/>
+            <input
+              className="gap"
+              value={cakeName}
+              type="text"
+              onChange={(e) => {
+                setCakeName(e.target.value);
+              }}
+            />
           </label>
           <label>
             <h4> Image-Url</h4>
-            <input className="gap" type="url"  onChange={handleImageUrl}/>
+            <input
+              className="gap"
+              type="url"
+              value={imageUrl}
+              onChange={(e) => {
+                setImageUrl(e.target.value);
+              }}
+            />
           </label>
           <label>
             <h4> Number of KGs</h4>
-            <input className="gap" type="number" onChange={handleNumber } />
+            <input
+              className="gap"
+              type="number"
+              value={numberOfKgs}
+              onChange={(e) => {
+                setNumberOfKgs(e.target.value);
+              }}
+            />
           </label>
           <div className="btn">
-           <input type='submit'/>
+            <input onClick={updateCakes} type="submit" />
           </div>
-         
         </form>
       </div>
     </div>
